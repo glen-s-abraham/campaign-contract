0358e50163b50f9f2678492adfd23b63924f71a4
glen <glenprojects666@gmail.com>
2024-06-01 11:02:26 +0530

    few typo fixes and deploy script addition

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -4,8 +4,8 @@ const { Web3 } = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-const campiledFactory = require('../ethereum/build/CampaignFactory.json');
-const campiledCampaign = require('../ethereum/build/Campaign.json');
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
 
 let accounts;
 let factory;
@@ -14,14 +14,14 @@ let campaign;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-    factory = await new web3.eth.Contract(JSON.parse(campiledFactory.interface))
-        .deploy({ data: campiledFactory.bytecode })
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: compiledFactory.bytecode })
         .send({ from: accounts[0], gas: '1000000' });
     await factory.methods.createCampaign('100').send({ from: accounts[0], gas: '1000000' });
 
     [campaignAddress] = await factory.methods.getDeployedCampaigns('100').call();
 
-    campaign = await new web3.eth.Contract(JSON.parse(campiledCampaign.interface), campaignAddress)
+    campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress)
 
 })
 
