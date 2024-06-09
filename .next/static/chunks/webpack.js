81b081c10051e7e179ec4a609328ee8c280e5200
glen <glenprojects666@gmail.com>
2024-06-09 12:19:59 +0530

    added campaign creation logic

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9794892829f60c5e"; }
+/******/ 		__webpack_require__.h = function() { return "5efdf6bdb64eb24a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Header from './Header'
 import { Container } from 'semantic-ui-react'
+import Head from 'next/head'
+
+
 
 export default function Layout({ children }) {
     return (
-        <Container>
-            <Header/>
-            {children}
-            <h1>Footer</h1>
-        </Container>
+        <>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+                />
+            </Head>
+            <Container>
+                <Header />
+                {children}
+            </Container>
+        </>
+
     )
 }
