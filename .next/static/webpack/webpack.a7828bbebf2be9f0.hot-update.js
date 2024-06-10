5b30943f699c844eb01283d73d19914e600ab4ef
glen <glenprojects666@gmail.com>
2024-06-10 18:32:03 +0530

    form setup

--- .next/static/webpack/webpack.a7828bbebf2be9f0.hot-update.js
+++ .next/static/webpack/webpack.a7828bbebf2be9f0.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7d0b2e2c88a82963"; }
+/******/ 	__webpack_require__.h = function() { return "1da3b7155b864871"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
