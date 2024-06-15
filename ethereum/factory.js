febdf1698ccac129dd2b090e050f9fd1d112179f
glen <glenprojects666@gmail.com>
2024-06-15 12:18:11 +0530

    added campaing listing component

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import CampaignFactory from './build/CampaignFactory.json'
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24'
+    '0x4bf3A7dFB3b76b5B3E169ACE65f888A4b4FCa5Ee'
 );
 
 export default instance;
