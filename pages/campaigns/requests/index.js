be0ee0195b02d18d3f7384b15095b6b8cf1c4153
glen <glenprojects666@gmail.com>
2024-06-18 18:49:28 +0530

    requests listing

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '../../../components/Layout';
 import { Link } from '../../../routes';
-import { Button } from 'semantic-ui-react';
+import { Button, Header, Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from 'semantic-ui-react';
+import getCampaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
 
-export default function requestIndex({ initialAddress }) {
+export default function requestIndex({ initialAddress, requests, requestCount, approversCount }) {
     return (
         <Layout>
             <h3>
@@ -14,6 +16,33 @@ export default function requestIndex({ initialAddress }) {
                     <Button primary>Add Request</Button>
                 </a>
             </Link>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderCell>ID</TableHeaderCell>
+                        <TableHeaderCell>Description</TableHeaderCell>
+                        <TableHeaderCell>Amount</TableHeaderCell>
+                        <TableHeaderCell>Recipient</TableHeaderCell>
+                        <TableHeaderCell>Approvals Count</TableHeaderCell>
+                        <TableHeaderCell>Approval</TableHeaderCell>
+                        <TableHeaderCell>Finalize</TableHeaderCell>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {requests.map((request, idx) => (
+                        <TableRow key={idx}>
+                            <TableCell>{idx}</TableCell>
+                            <TableCell>{request.description}</TableCell>
+                            <TableCell>{web3.utils.fromWei(request.value, 'ether')}</TableCell>
+                            <TableCell>{request.recipient}</TableCell>
+                            <TableCell>{request.approvalsCount}/{approversCount}</TableCell>
+                            <TableCell><Button>Approve</Button></TableCell>
+                            <TableCell><Button>Finalize</Button></TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+
+            </Table>
         </Layout>
     )
 }
@@ -21,9 +50,23 @@ export default function requestIndex({ initialAddress }) {
 // Fetch the query parameters on the server side
 export async function getServerSideProps(context) {
     const { address } = context.query;
+    const campaign = getCampaign(address);
+    const requestsCount = await campaign.methods.getRequestsCount().call();
+    const requests = await Promise.all(Array(requestsCount).fill().map((e, i) => campaign.methods.requests(i).call()))
+    const approversCount = await campaign.methods.approversCount().call()
+    const serializedResult = requests.map(item => ({
+        description: item.description,
+        value: item.value.toString(), // Ensure value is a string
+        recipient: item.recipient,
+        completed: item.completed,
+        approvalsCount: item.approvalsCount.toString(), // Ensure approvalsCount is a string
+    }));
     return {
         props: {
             initialAddress: address || null,
+            requests: serializedResult || null,
+            requestsCount: requestsCount || 0,
+            approversCount: approversCount || 0
         },
     };
 }
